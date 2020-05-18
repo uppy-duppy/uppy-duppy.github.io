211c26341b8ab9c44ab26813fb7cc72acd5fa261
uppy-duppy <55457619+uppy-duppy@users.noreply.github.com>
2020-05-18 22:23:53 +0300

    Add files via upload

---
+++ aleynaDg.js
@@ -0,0 +1,17 @@
+const BOX = document.getElementById('toChanged');
+const BUTTON = document.getElementById('buttonSec');
+
+BUTTON.addEventListener('click', function(){window.open('file:///Users/macbookpro/Desktop/dogumGunu%20Aleyna/maymunPopUp.html');},false);
+BOX.addEventListener('click', changeImage, false);
+ function changeImage(e){
+   e.preventDefault();
+  if(BOX.src = 'happy.png'){
+    BOX.src='imgAleyna.png';
+    BOX.style.transform= 'rotate(90deg)';
+    BOX.style.width= '500px';
+    BOX.style.height= '700px';
+    BOX.style.left= '600px';
+    BOX.style.top= '50px';
+
+  }
+ }
